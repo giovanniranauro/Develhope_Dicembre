79b922e84981bd47e7fcf7bbf847a3c6d9979171
giovanniranauro <gio@Air-di-Giovanni.station>
2023-12-18 19:57:47 +0100

    update

---
+++ Destrutturazione/destrutt2.js
@@ -0,0 +1,18 @@
+//Dato uno studente oggetto nidificato con informazioni sulle proprietà e voti, 
+//utilizza la destrutturazione dell'oggetto 
+//per creare variabili nome, età e voto che contengano i valori dell'oggetto nidificato.
+
+
+
+
+const student = {
+  info: { name: "Bob", age: 25 },
+  grades: { math: 95, science: 89 }
+};
+
+const {name,age} = student.info
+const {math,science} = student.grades
+
+console.log(name,age)
+console.log(math,science);
+
