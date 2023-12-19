58aa22d6326a285b5c572e728c59b7f85b60a36b
giovanniranauro <gio@Air-di-Giovanni.station>
2023-12-19 17:14:33 +0100

    update es.destrutturazione3

---
+++ Destrutturazione/destr3.js
@@ -0,0 +1,15 @@
+// Dato un array di colori con tre elementi, 
+// utilizza la destrutturazione dell'array per 
+// creare variabili firstColor, secondColor e ThirdColor 
+// che contengono i valori dell'array.
+
+
+const colors = ["red", "green", "blue"];
+
+const [firstColor,secondColor,ThirdColor] = colors
+
+console.log(firstColor)
+console.log(secondColor)
+console.log(ThirdColor)
+
+
