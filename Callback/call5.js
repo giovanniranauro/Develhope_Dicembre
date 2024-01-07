c3843adc09a87627d509be4449cbc49a22e2b96c
giovanniranauro <gio@Air-di-Giovanni.station>
2024-01-07 19:57:09 +0100

    aggiunto es.5

---
+++ Callback/call5.js
@@ -0,0 +1,16 @@
+// Crea una funzione che accetta due numeri come argomenti e una funzione di callback.
+// La funzione dovrebbe eseguire un'operazione matematica sui numeri e passare il risultato alla funzione di callback.
+// Ad esempio, puoi creare una funzione per aggiungere due numeri e passare una funzione di callback che visualizza il risultato.
+
+
+
+function performOperation(a, b, callback) {
+  const result = a + b
+  callback(result)
+}
+
+function displayResult(result) {
+    console.log(`Il risultato dell'operazione è: ${result}`)
+}
+
+performOperation(5, 3, displayResult);
